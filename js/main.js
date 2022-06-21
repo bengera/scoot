631d505f073004cc3df5fb6e7169ce7735d3cfb8
unknown <bengera89@gmail.com>
2022-06-21 14:27:24 +0700

    chore🧹

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@ hamMenu.addEventListener('click',() => {
     console.log('toggle-menu');
     hamMenu.classList.toggle('active');
     navPrimary.classList.toggle('show');
+    overlay.setAttribute("style", "display:block");
+
+    if (hamMenu.classList.contains('active')) {
+        console.log('menu-open');
+
+    } else {
+        overlay.setAttribute("style", "display:none"); 
+        // resetMenu(); // reset drop-downs
+    }
     
     
 });
