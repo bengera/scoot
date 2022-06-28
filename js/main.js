be18166c572269ec2995cbef913b65f55e9d64c9
unknown <bengera89@gmail.com>
2022-06-28 14:12:04 +0700

    tablet-how-to

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const navPrimary = document.querySelector('.nav-primary');
 
-hamMenu.addEventListener('click',() => {
+hamMenu.addEventListener('click', () => {
     console.log('toggle-menu');
     hamMenu.classList.toggle('active');
     navPrimary.classList.toggle('show');
@@ -12,11 +12,11 @@ hamMenu.addEventListener('click',() => {
         document.body.setAttribute("style", "position:fixed");
 
     } else {
-        overlay.setAttribute("style", "display:none"); 
+        overlay.setAttribute("style", "display:none");
         document.body.setAttribute("style", "position:static");
     }
-    
-    
+
+
 });
 
 
@@ -25,17 +25,34 @@ function scrollDownload() {
     let downloadSection = document.querySelector(".footer");
     downloadSection.scrollIntoView(true);
     hamMenu.classList.remove('active');
-    
-        
+
+
 }
 
 
 function resetMenu() {
     console.log('reset');
     document.body.setAttribute("style", "position:static");
-    overlay.setAttribute("style", "display:none"); 
+    overlay.setAttribute("style", "display:none");
     hamMenu.classList.toggle('active');
     navPrimary.classList.remove('show');
-    
+
 
 }
+
+(function () {
+    window.onresize = displayWindowSize;
+    window.onload = displayWindowSize;
+
+    function displayWindowSize() {
+        let myWidth = window.innerWidth;
+
+        if (myWidth >= 600) {
+            if (hamMenu.classList.contains('active')) {
+                resetMenu();
+            }
+            
+        } 
+
+    }
+})();
